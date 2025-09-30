1380ba520c06ff68a03a3a8a446d2b50760eb8a7
Min-hong-gi <borin@openobject.net>
2025-09-30 09:41:17 +0900

    불필요한 파일 삭제

--- dist/util/common.utils.js
+++
@@ -1,8 +0,0 @@
-export function parseTemplate(strings, values) {
-    if (strings.length === 1)
-        return Number(strings[0]);
-    return values[0];
-}
-export function lerp(a, b, t) {
-    return a + (b - a) * t;
-}
--- dist/util/shape.util.js
+++
@@ -1,8 +0,0 @@
-export function deg(strings, ...values) {
-    let val;
-    val = values[0];
-    if (strings.length == 1) {
-        val = new Number(strings[0]).valueOf();
-    }
-    return Math.PI / 180 * val;
-}
